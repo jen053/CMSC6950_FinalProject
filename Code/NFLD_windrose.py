9d048a2289e72f8123a882035e59396178f22136
Jacob_Newman <jen053@mun.ca>
2020-07-27 16:37:15 -0230

    Added basemap function.

--- Code/NFLD_windrose.py
+++ Code/NFLD_windrose.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-
+from mpl_toolkits.basemap import Basemap
 from windrose import WindroseAxes, WindAxes, plot_windrose
 
 
-def plot(filename, location, cmap, bin_interval=10, plt_type='bar', saveImage=False):
+def plotWindrose(filename, location, cmap, bin_interval=10, plt_type='bar', saveImage=False):
     """
     Plotting function to generate windrose (polar) diagrams for provided wind speed / directional data.
     Usage:
@@ -41,13 +41,35 @@ def plot(filename, location, cmap, bin_interval=10, plt_type='bar', saveImage=Fa
         ax.set_title('Wind Polar Plot', fontsize=16)
     ax.set_legend(ncol=2, title='Wind Speed (km/h)')
     if saveImage:
-        plt.savefig('C:/Users/Jacob/CMSC6950_py/Open_Science_Project/CMSC6950_FinalProject/Images/'+location+'.pdf')
+        plt.savefig('C:/Users/Jacob/CMSC6950_py/Open_Science_Project/CMSC6950_FinalProject/Images/' + location + '.pdf')
 
     plt.show()
     return None
 
 
-plot('C:/Users/Jacob/CMSC6950_py/Open_Science_Project/CMSC6950_FinalProject/Data/Gander_1980-2012.csv',
-     location="Gander Airport",
-     bin_interval=10,
-     plt_type='contourf', cmap=cm.hot, saveImage=True)
+def plotMap(mapbounds, lats, lons, lat_0, lon_0):
+    """
+    
+    """
+    cords = []
+    plt.figure(figsize=(10, 10))
+    map = [Basemap(i, j, k, l, resolution='i', projection='tmerc', lat_0=lat_0, lon_0=lon_0)
+           for i, j, k, l in mapbounds]
+    m = map[0]
+    [cords.append(m(i, j)) for i, j in zip(lons, lats)]
+
+    m.drawmapboundary(fill_color='aqua')
+    m.fillcontinents(color='coral', lake_color='aqua')
+    m.drawcoastlines()
+    [m.plot(x, y, marker='o', color='red', markersize=10) for x, y in cords]
+
+    plt.show()
+
+
+plotWindrose('C:/Users/Jacob/CMSC6950_py/Open_Science_Project/CMSC6950_FinalProject/Data/Gander_1980-2012.csv',
+             location="Gander Airport",
+             bin_interval=10,
+             plt_type='contourf', cmap=cm.hot, saveImage=False)
+
+plotMap(mapbounds=[[-59.5, 46, -52.5, 52]], lats=[47.6212, 47.3, 49.22, 48.95, 48.53],
+        lons=[-52.7423, -54, -57.4, -54.58, -58.55], lat_0=47, lon_0=-53)
