1418fca0b0c21fb035124389b87368c60cbf09be
Jacob_Newman <jen053@mun.ca>
2020-07-31 11:25:31 -0230

    Python file to access wind data for NL airports

---
+++ Code/getData.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import wget
+
+
+def getData(location):
+    url = 'https://raw.githubusercontent.com/jen053/windData/master/' + location + '.csv'
+
+    wget.download(url, 'c:/Users/Jacob/CMSC6950_py/Open_Science_Project/CMSC6950_FinalProject/data.txt')
+
+    return None
+
+
+def main(args):
+    if len(args) != 2:
+        raise SystemExit('Stopped')
+
+    getData(location=args[1])
+
+
+if __name__ == '__main__':
+    import sys
+
+    main(sys.argv)
