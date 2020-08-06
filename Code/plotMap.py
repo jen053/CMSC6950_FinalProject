f01c724a6dd504accb2649d5aca7eee73bb40248
Jacob_Newman <jen053@mun.ca>
2020-08-06 09:57:50 -0230

    Added scatter for other locations.

--- Code/plotMap.py
+++ Code/plotMap.py
@@ -43,13 +43,20 @@ def plotMap(loc):
                 (48.53, -58.55),
                 (52.93, -66.87)]
 
-    locations = dict(zip(places, lat_lons))
+    locations = dict(zip(places, lat_lons))  # Locations of all airports
+    other_locs = locations.copy()
+    del other_locs[loc]  # Locations of all airports except the user-defined airport (for plotting)
 
     plt.figure(figsize=(20, 15))
-    mapbounds = [[-63, 46, -52, 60]]
+    mapbounds = [[-66.25, 46, -52, 60]]
     map = [Basemap(i, j, k, l, resolution='i', projection='tmerc', lat_0=47, lon_0=-53) for i, j, k, l in mapbounds]
     m = map[0]
+
+    # Converting from lat-lon to map coordinates
     coords = m(locations[loc][1], locations[loc][0])
+    other_coords = []
+    for k in other_locs:
+        other_coords.append(m(other_locs[k][1], other_locs[k][0]))
 
     m.drawmapboundary(fill_color='aqua')
     m.fillcontinents(color='coral', lake_color='aqua')
@@ -57,12 +64,14 @@ def plotMap(loc):
     m.drawparallels(np.arange(40, 60, 1), labels=[1, 0, 0, 0])
     m.drawmeridians(np.arange(-65, -50, 1), labels=[0, 0, 0, 1])
     m.plot(coords[0], coords[1], marker='*', color='yellow', markersize=15)
+    [m.plot(x, y, marker='o', color='k', markersize=8) for x, y in other_coords]
     plt.xlabel('Longitude', labelpad=40, fontsize=12)
     plt.ylabel('Latitude', labelpad=40, fontsize=12)
     plt.savefig('location.pdf', pad_inches=0, bbox_inches='tight')
     plt.show()
 
-#plotMap('St.Johns_2012-2019')
+
+plotMap('Buchans_1953-1965')
 
 def main(args):
     if len(args) != 2:
